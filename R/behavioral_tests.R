58d492b11a1a55ee8092d23400d50bb60f6a3da8
ecamenen <etienne.camenen@gmail.com>
2022-03-04 12:18:00 +0100

    Add package data

---
+++ R/behavioral_tests.R
@@ -0,0 +1,17 @@
+#' Behavioral tests
+#'
+#' Behavioral tests of mice with immune cell transplantation from multiple sclerosis patients.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name behavioral_tests
+#' @usage data(behavioral_tests_with_replicates)
+"behavioral_tests_with_replicates"
+
+#' @rdname behavioral_tests
+#' @usage data(behavioral_tests_no_replicates_median)
+"behavioral_tests_no_replicates_median"
+
+#' @rdname behavioral_tests
+#' @usage data(behavioral_tests_no_replicates)
+"behavioral_tests_no_replicates"
