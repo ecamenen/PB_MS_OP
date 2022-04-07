ae70448fff857b9a66709de20c54cbd815315e65
ecamenen <etienne.camenen@gmail.com>
2022-04-07 18:54:47 +0200

    Add load_libraries, modify fig dim for vignettes

--- R/perrot-package.R
+++ R/perrot-package.R
@@ -3,7 +3,8 @@
 
 ## usethis namespace: start
 #' @import bookdown buildmer car corrplot emmeans factoextra FactoMineR
-#' @import ggplot2 ggpubr ggResidpanel kableExtra lmerTest pander readxl
+#' @import ggplot2 ggpubr ggResidpanel kableExtra lmerTest openxlsx
+#' @import pander readxl reshape2 RGCCA rstatix texreg tidyverse wesanderson
 #' @import reshape2 RGCCA rstatix texreg tidyverse wesanderson
 #' @rawNamespace import(lme4, except = lmer)
 #' @rawNamespace import(magrittr, except = extract)
